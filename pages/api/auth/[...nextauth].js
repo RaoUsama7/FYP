df0f777e1ea374fe93cc89e9256e8d2a5507f576
Usama <raorvp20@gmail.com>
2023-02-03 03:18:09 +0500

    authentication completed with minor error

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,55 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb"
+import CredentialsProvider from "next-auth/providers/credentials";
+import loginUser from '@utils/database'// I want to take my own function from postLogin and SignUp
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          // Return all the profile information you need.
+          // The only truly required field is `id`
+          // to be able identify the account when added to a database
+        }
+      },
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+          placeholder: 'username'
+        },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials
+        const user = await loginUser(email, password)
+ 
+        if (user) {
+          return user
+        } else {
+          return null
+        }
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt",
+    
+  },
+  pages: {
+    signIn: '/pages/LogIn',
+    newUser: '/pages/SignUp'
+  }
+}
+
+export default NextAuth(authOptions)
